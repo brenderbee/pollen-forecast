9d6205f9e9de8ec0f57f12215af8df9992b8c355
Brenda Franco <brenda.leigh.franco@gmail.com>
2018-07-13 16:33:50 -0700

    Took break for the gym and to commute to school. Currently learning scrolling in React Native.

--- Pollen/App.js
+++ Pollen/App.js
@@ -1,59 +1,17 @@
 import React, { Component } from 'react';
-import { Alert, AppRegistry, Button, StyleSheet, View } from 'react-native';
+import { AppRegistry, ScrollView, Image, Text } from 'react-native';
 
-export default class ButtonBasics extends Component {
-  _onPressButton() {
-    Alert.alert('You tapped the button!')
-  }
-
+export default class IScrolledDownAndWhatHappenedNextShockedMe extends Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <View style={styles.buttonContainer}>
-          <Button
-            onPress={this._onPressButton}
-            title="Press Me"
-          />
-        </View>
-        <View style={styles.buttonContainer}>
-          <Button
-            onPress={this._onPressButton}
-            title="Press Me"
-            color="#841584"
-          />
-        </View>
-        <View style={styles.alternativeLayoutButtonContainer}>
-          <Button
-            onPress={this._onPressButton}
-            title="This looks great!"
-          />
-          <Button
-            onPress={this._onPressButton}
-            title="Learn More"
-            color="#fff"
-            accessibilityLabel="Learn more about this purple button"
-          />
-        </View>
-      </View>
+      return (
+        <ScrollView>
+          <Text style={{fontSize:96}}>Scroll me plz</Text>
+          <Text style={{fontSize:96}}>If you like</Text>
+          <Text style={{fontSize:96}}>Scrolling down</Text>
+          <Text style={{fontSize:96}}>What's the best</Text>
+          <Text style={{fontSize:96}}>Framework around?</Text>
+          <Text style={{fontSize:80}}>React Native</Text>
+        </ScrollView>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-   flex: 1,
-   justifyContent: 'center',
-  },
-  buttonContainer: {
-    color: '#fff',
-    backgroundColor: '#9f67ff',
-    margin: 20
-  },
-  alternativeLayoutButtonContainer: {
-    color: '#fff',
-    margin: 20,
-    backgroundColor: '#9f67ff',
-    flexDirection: 'row',
-    justifyContent: 'space-between'
-  }
-})
